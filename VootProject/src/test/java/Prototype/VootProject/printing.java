bbeddad21e602bbe281b92e93ce11451cadaad0e
Nitesh <Nitesh@DESKTOP-077G3EM>
2022-07-04 13:39:21 +0530

    committed 2nd time

--- VootProject/src/test/java/Prototype/VootProject/printing.java
+++ VootProject/src/test/java/Prototype/VootProject/printing.java
@@ -3,7 +3,7 @@ package Prototype.VootProject;
 public class printing {
 
 	public static void main(String[] args) {
-		System.out.println("My new project");
+		System.out.println("My new project is Voot");
 
 	}
 
