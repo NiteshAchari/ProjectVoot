b8cbd3f7f159ffaa12ebea6721aed7e083b7bea8
Nitesh <Nitesh@DESKTOP-077G3EM>
2022-07-04 13:23:07 +0530

    commited first time

---
+++ VootProject/src/main/java/Prototype/VootProject/App.java
@@ -0,0 +1,7 @@
+package Prototype.VootProject;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ VootProject/src/test/java/Prototype/VootProject/printing.java
@@ -0,0 +1,10 @@
+package Prototype.VootProject;
+
+public class printing {
+
+	public static void main(String[] args) {
+		System.out.println("My new project");
+
+	}
+
+}
